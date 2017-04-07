0411620f29060a0a68737dcc3ed4ca971c462bff
Ayda Sulca <suni_yuri_94@hotmail.com>
2017-04-07 17:55:46 -0500

    primerCommit

---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+document.getElementById("toggle").addEventListener("click", function(event){
+  event.preventDefault();
+  document.getElementById("nav-header").classList.toggle("open");
+  document.getElementById("body").classList.toggle("overflow-hidden");
+});
+//completar
+/*
+var currentScroll = window.pageYOffset || document.body.scrolltop;
+var lastScrollTop = 0;
+window.addEventListener("color", function(){
+  var currentScroll = window.pageYOffset || document.body.scrolltop;
+}, false);
+window.addEventListener("scroll", function(){
+  var currentScroll = window.pageYOffset || document.body.scrolltop;
+  if (currentScroll > lastScrollTop){
+    console.log("down");
+  }else{
+    console.log("up");
+  }
+  lastScrollTop = currentScroll;
+})
+*/
